55c72282541bd29835fbc9628fbe812b24a4399c
Tanish Singhal <tanishsinghal510@gmail.com>
2024-12-05 21:38:15 +0530

    GEC questions

---
+++ Global Execution Context/1-hosting.js
@@ -0,0 +1,13 @@
+x = 30;
+
+console.log(`Value of X is ${x}`);
+
+var x = 10;
+
+console.log(`Value of X is ${x}`);
+
+// in case of var, the variable is hoisted to the top
+
+// but in let and const the variable is not hoisted to the top
+
+// but let and const are also hoisted to the top but in a different way (temporal dead zone)
